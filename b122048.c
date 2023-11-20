1dc18e707d417e3c7484b4213d8efce644dbc444
Venkat Raju <118790430+Venkatknight2295@users.noreply.github.com>
2023-11-20 18:44:53 +0530

    b122048

--- b122048.c
+++ b122048.c
@@ -1,5 +1,12 @@
-#include<stdio.h>
+/*
+Name: G.N.Venkata Pathi Raju
+ID  : B122048
+This Code Prints Hello World in cpp :D
+*/
+
+#include<iostream>
+using namespace std;
 int main(){
-    printf("hello world");
+    cout<<"hello"<<endl;
     return 0;
 }
