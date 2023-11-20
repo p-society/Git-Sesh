963dd0fcecf891fa1f5a8438d9d2f4d4ae96c3af
Soubhik Gon <119437069+zakhaev26@users.noreply.github.com>
2023-11-20 17:46:56 +0530

    Create B422056.go

---
+++ B422056.go
@@ -0,0 +1,19 @@
+/*
+Name: Soubhik Gon
+ID  : B422056
+This Code Prints Hello World in Go :D
+*/
+
+package main
+
+import "fmt"
+
+func printText() {
+  fmt.Println("Hello World ;) ");
+}
+
+func main() {
+    for i:=0;i<69;i++ {
+      printText();
+    }
+}
