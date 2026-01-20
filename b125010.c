432592c75d7db3f98de00dd32c39082d39334198
amitesh-0 <amiteshsarangi@gmail.com>
2026-01-20 19:43:03 +0530

    added my file

--- b125010.c
+++ b125010.c
@@ -2,7 +2,7 @@
 Name:Amitesh Sarangi
 ID  : B125010
 This Code Prints Hello World in C :D
-I dont this i will be getting any Microsoft workshop certificate though :O
+I dont i will be getting any Microsoft workshop certificate though :O
 */
 
 #include <stdio.h>
